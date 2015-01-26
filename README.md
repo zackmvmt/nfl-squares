b9beef4090c8561348c03d17e5937cbf3dd0cf93
Zack Stickles <zack@movementstrategy.com>
2015-01-26 23:56:36 +0000

    started work on setting up boilderplate

---
+++ README.md
@@ -0,0 +1,8 @@
+# Simple Node Boilerplate - ZSTIX
+
+## TODO
+  - set up sass / gulp (run node and compile scss)
+  - document how to start working with the files
+  - document how to push up to heroku
+  - example route
+  - example / documentation for DB calls (if needed)
---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+app.set('view engine', 'jade');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) {
+  res.render('index', { title: 'Express View Test', message: 'Hello Jade!' });
+});
+
+var server = app.listen(3000, function() {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
+});
