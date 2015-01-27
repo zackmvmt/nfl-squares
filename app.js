455ea62bb43bb9d152732f7af9f9c785b0eabf61
Zack Stickles <zack@movementstrategy.com>
2015-01-27 22:02:47 +0000

    setting up the ports for heroku

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+var port = process.env.PORT || 3000;
 
 app.set('view engine', 'jade');
 app.use(express.static(path.join(__dirname, 'public')));
@@ -9,7 +10,7 @@ app.get('/', function(req, res) {
   res.render('index', { title: 'Nodemon View Test', message: 'Hello Jade!' });
 });
 
-var server = app.listen(3000, function() {
+var server = app.listen(port, function() {
   var host = server.address().address;
   var port = server.address().port;
   console.log('Example app listening at http://%s:%s', host, port);
