55aedd12be7a1b5e099bdae0e6f336342c653bd4
Zack Stickles <zack@movementstrategy.com>
2015-01-30 16:29:37 -0700

    added pages and some starter styles

--- public/js/main.js
+++ public/js/main.js
@@ -3,5 +3,16 @@ $(function() {
   // $.get('/api/users', function(data) {
   //   console.log('users', data);
   // });
+
+
+	nextPage = function() {
+		$('.content').animate({ 'margin-left': '-=800' }, 500);
+	};
+	prevPage = function() {
+		$('.content').animate({ 'margin-left': '+=800' }, 500);
+	};
+	$('.pick-squares-btn').on('click', nextPage);
+	$('.start-game-btn').on('click', nextPage);
+	$('.settings-btn').on('click', prevPage);
   
 });
